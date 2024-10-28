b7d9287a8d722506dcc1bad7511b15349bc6a1ea
Vuk7 <vukemerik@gmail.com>
2024-10-28 20:55:09 +0100

    feat: add main screen

--- src/lib/screens/main_screen/main_screen.dart
+++ src/lib/screens/main_screen/main_screen.dart
@@ -1,3 +1,6 @@
+import 'package:bela_blok/screens/main_screen/widgets/history_list_item.dart';
+import 'package:bela_blok/screens/widgets/big_button.dart';
+import 'package:bela_blok/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -10,6 +13,76 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: SafeArea(
+            child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Center(
+        child: Column(
+          children: [
+            Text(
+              "BELA BLOK",
+              style: TextStyle(
+                  fontSize: 38,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Povijest:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            HistoryListItem(
+                gameID: 0,
+                date: "21.10.2024 19:44",
+                teamOneScore: 1030,
+                teamTwoScore: 560,
+                onTap: () {}),
+            HistoryListItem(
+                gameID: 0,
+                date: "21.10.2024 19:44",
+                teamOneScore: 1030,
+                teamTwoScore: 560,
+                onTap: () {}),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: BigButton(
+                text: "NASTAVI",
+                textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold),
+                bgColor: AppTheme.red,
+                onTap: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: BigButton(
+                text: "NOVA IGRA",
+                textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold),
+                bgColor: AppTheme.green,
+                onTap: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    )));
   }
 }
---
+++ src/lib/screens/main_screen/widgets/history_list_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HistoryListItem extends StatelessWidget {
+  final String date;
+  final int gameID, teamOneScore, teamTwoScore;
+  final Function() onTap;
+
+  const HistoryListItem({
+    super.key,
+    required this.gameID,
+    required this.date,
+    required this.teamOneScore,
+    required this.teamTwoScore,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                date,
+                style: const TextStyle(fontSize: 15),
+              ),
+              const Spacer(),
+              Text(
+                "$teamOneScore - $teamTwoScore",
+                style: const TextStyle(fontSize: 20),
+              ),
+              const Spacer(),
+              const Text(
+                ">",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          const Divider()
+        ],
+      ),
+    );
+  }
+}
