69971b7a96d27e4df9a5383c8240448315408ac3
Vuk7 <vukemerik@gmail.com>
2024-11-10 12:02:11 +0100

    feat: dummy data for round list

--- src/lib/screens/current_game_screen/current_game_screen.dart
+++ src/lib/screens/current_game_screen/current_game_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:bela_blok/enums/caller_enum.dart';
 import 'package:bela_blok/screens/current_game_screen/widgets/round_score_list_item.dart';
 import 'package:bela_blok/screens/current_game_screen/widgets/top_score_details.dart';
+import 'package:bela_blok/screens/widgets/big_button.dart';
+import 'package:bela_blok/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CurrentGameScreen extends StatefulWidget {
@@ -11,10 +15,39 @@ class CurrentGameScreen extends StatefulWidget {
 }
 
 class _CurrentGameScreenState extends State<CurrentGameScreen> {
+  final ScrollController _scrollController = ScrollController();
+
   Map<Caller, int> teamScore = {
     Caller.teamOne: 980,
     Caller.teamTwo: 990,
   };
+
+  List<Map<String, dynamic>> dummyRoundData = List.generate(10, (index) {
+    return {
+      'teamOneCallAmount': 0,
+      'teamTwoCallAmount': 20,
+      'teamOneScore': 62,
+      'teamTwoScore': 100,
+      'roundID': index,
+      'teamCalled': Caller.teamTwo
+    };
+  });
+
+  void _scrollToEnd() {
+    if (_scrollController.hasClients) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,18 +71,51 @@ class _CurrentGameScreenState extends State<CurrentGameScreen> {
           const SizedBox(
             height: 10,
           ),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: RoundScoreListItem(
-              teamOneCallAmount: 20,
-              teamTwoCallAmount: 0,
-              teamOneScore: 100,
-              teamTwoScore: 82,
-              roundID: 0,
-              teamCalled: Caller.teamOne,
-              onTap: () {},
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              padding: const EdgeInsets.all(5.0),
+              itemCount: dummyRoundData.length,
+              itemBuilder: (context, index) {
+                final round = dummyRoundData[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: RoundScoreListItem(
+                    teamOneCallAmount: round['teamOneCallAmount'],
+                    teamTwoCallAmount: round['teamTwoCallAmount'],
+                    teamOneScore: round['teamOneScore'],
+                    teamTwoScore: round['teamTwoScore'],
+                    roundID: round['roundID'],
+                    teamCalled: round['teamCalled'],
+                    onTap: () {},
+                  ),
+                );
+              },
             ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: BigButton(
+              text: "DODAJ",
+              textStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold),
+              bgColor: AppTheme.green,
+              onTap: () {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  _scrollToEnd();
+                });
+              },
+              textPadding: 15,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          )
         ]),
       ),
     )));
