ef92f09cefa7e1efae8320b4c85853b368731e1d
Soky0-0 <msokser529@gmail.com>
2025-07-31 16:42:45 +0200

    featrue: fix to adut logic

--- src/lib/screens/smart_calculator_screen/smart_calculator_screen.dart
+++ src/lib/screens/smart_calculator_screen/smart_calculator_screen.dart
@@ -92,14 +92,31 @@ class _SmartCalculatorScreenState extends State<SmartCalculatorScreen> {
 
   void _toggleTrumpCard(PlayingCard card) {
     setState(() {
-      if (_selectedCards.contains(card)) {
-        if (_trumpCards.contains(card.id)) {
-          _trumpCards.remove(card.id);
-        } else {
-          _trumpCards.add(card.id);
-        }
-        _calculateScore();
+      // Dozvoli označavanje samo za 9 i J koji su odabrani
+      if (!_selectedCards.contains(card) || !(card.rank == '9' || card.rank == 'J')) {
+        return;
       }
+
+      // Provjeri je li karta već označena kao adut
+      if (_trumpCards.contains(card.id)) {
+        _trumpCards.remove(card.id);
+      } else {
+        // Pronađi sve adute (9 i J) u drugim bojama
+        final currentSuit = card.suit;
+        final adutIdsInOtherSuits = _trumpCards.where((id) {
+          final parts = id.split('_');
+          return parts.length == 2 && parts[1] != currentSuit && (parts[0] == '9' || parts[0] == 'J');
+        }).toList();
+
+        // Ukloni sve adute iz drugih boja
+        for (final id in adutIdsInOtherSuits) {
+          _trumpCards.remove(id);
+        }
+
+        // Dodaj oznaku aduta samo za kliknutu kartu
+        _trumpCards.add(card.id);
+      }
+      _calculateScore();
     });
   }
 
@@ -323,7 +340,7 @@ class _SmartCalculatorScreenState extends State<SmartCalculatorScreen> {
 
           return GestureDetector(
             onTap: () => _toggleCardSelection(card),
-            onLongPress: () => _toggleTrumpCard(card),
+            onLongPress: (card.rank == '9' || card.rank == 'J') ? () => _toggleTrumpCard(card) : null,
             child: Container(
               decoration: BoxDecoration(
                 border: Border.all(
