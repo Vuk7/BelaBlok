0cc994aef9a92ada5fc9e9bb5cd6f6fb27df04c3
msokser22 <162346998+MarkoSokser@users.noreply.github.com>
2026-01-06 10:09:08 +0100

    fix: max input for base score implemented

--- src/lib/screens/widgets/big_button_input_number.dart
+++ src/lib/screens/widgets/big_button_input_number.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+class MaxValueInputFormatter extends TextInputFormatter {
+  final int maxValue;
+
+  MaxValueInputFormatter(this.maxValue);
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    if (newValue.text.isEmpty) {
+      return newValue;
+    }
+
+    final int? value = int.tryParse(newValue.text);
+    if (value == null) {
+      return oldValue;
+    }
+
+    if (value > maxValue) {
+      return TextEditingValue(
+        text: maxValue.toString(),
+        selection: TextSelection.collapsed(offset: maxValue.toString().length),
+      );
+    }
+
+    return newValue;
+  }
+}
+
 class BigButtonInputNumber extends StatelessWidget {
   final String text;
   final Color bgColor;
@@ -10,6 +40,7 @@ class BigButtonInputNumber extends StatelessWidget {
   final double textPadding;
   final double? width;
   final String? suffixText;
+  final int maxValue;
 
   const BigButtonInputNumber({
     super.key,
@@ -21,6 +52,7 @@ class BigButtonInputNumber extends StatelessWidget {
     this.textPadding = 20.0,
     this.width,
     this.suffixText,
+    this.maxValue = 252,
   });
 
   @override
@@ -52,7 +84,8 @@ class BigButtonInputNumber extends StatelessWidget {
           ),
           keyboardType: TextInputType.number,
           inputFormatters: <TextInputFormatter>[
-            FilteringTextInputFormatter.digitsOnly
+            FilteringTextInputFormatter.digitsOnly,
+            MaxValueInputFormatter(maxValue),
           ],
         ),
       ),
