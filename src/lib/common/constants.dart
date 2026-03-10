e710485217886cdf2041a1d65e554b6280c3ec28
msokser22 <msokser22@student.foi.hr>
2026-03-10 13:34:41 +0100

    fix: stack renderer on top of stack now (#74)
    * fix: stack renderer on top of stack now
    
    * fix: settings now load before loadRounds to fix inverting the list of rounds
    
    ---------
    
    Co-authored-by: Soky0-0 <msokser529@gmail.com>

--- src/lib/common/constants.dart
+++ src/lib/common/constants.dart
@@ -5,7 +5,7 @@ const String databaseName = "bela_blok";
 // App metadata
 const String appName = "Super Bela Blok";
 
-const String appVersion = '0.1.4';
+const String appVersion = '0.1.5';
 const String appRepositoryUrl = "https://github.com/Vuk7/BelaBlok";
 
 // Team display names
